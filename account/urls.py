6d32a99b05e5cc3a9d5a0b99bbddf713a14925f3
Tahira-Qadir <139474930+Tahira-Qadir@users.noreply.github.com>
2024-03-17 22:22:03 +0500

    Account Athentication

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import(
+from account.views import(
     home,
+    register_view,
+    login_view,
+    logout_view,
 )
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from .forms import RegistrationForm, AccountAuthenticationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def register_view(request):
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if  form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+            form = RegistrationForm()
+    return render(request, 'account/register.html', {
+        'form':form
+        })
+
+def login_view(request):
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form =  AccountAuthenticationForm()
+    return render(request,'account/login.html', {
+            'form' : form
+        })
+
+def  logout_view(request):
+    logout(request)
+    return redirect('login')
